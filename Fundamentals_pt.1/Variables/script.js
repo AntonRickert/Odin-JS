26a7f66ee731226e21b449cd5e74032691af0818
AntonRickert <antonrickert.dev@gmail.com>
2022-08-26 08:32:22 +0200

    Simple math practice in JavaScript

---
+++ Fundamentals_pt.1/Variables/script.js
@@ -0,0 +1,11 @@
+console.log(23 + 97);
+console.log(23 + 97 + 50 + 25 + 25 + 100);
+console.log((4 + 6 + 9) / 77);
+let a = 10;
+console.log(9 * a);
+let b = 7 * a;
+console.log(b);
+const max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+
